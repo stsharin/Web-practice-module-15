a7d043bfedf5c040feeb9305f885a394d44b183e
stsharin <stssharin@gmail.com>
2021-01-20 01:36:59 +0600

    first commit

---
+++ maxOfThree.js
@@ -0,0 +1,16 @@
+ var business = 550;
+ var minister = 650;
+ var sochib = 725;
+
+ var maxValue = Math.max(business, minister, sochib);
+ console.log(maxValue);
+
+ /** if(business>sochib && business>minister){
+     console.log("Business is bigger.");
+ }
+ else if (minister>business && minister>sochib){
+    console.log("Minister is bigger.");
+ }
+ else{
+    console.log("Sochib is bigger.");
+ } */
---
+++ random.js
@@ -0,0 +1,7 @@
+var num = 2;
+
+for (var i = 0; i < 10; i++) {
+    var randomNumber = Math.random() * 6;
+    var output = Math.round(randomNumber);
+    console.log(output);
+}
